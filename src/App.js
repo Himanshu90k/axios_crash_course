a421f9c5252606e923a2012cb102248f82eeb52c
Himanshu Rawat <himanshurawat90k@gmail.com>
2023-02-01 20:02:35 +0530

    re-writing Todo component.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { useState } from 'react';
+import Todos from './components/Todo';
+import api from './api/api_todo';
 
+const App = () =>{
+
+  return (
+    <>
+      <div className="container">
+        <Todos />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+const Todos = () => {
+    return (
+        <div className="Task-box">
+            <h1>Task List</h1>
+            <h2>One Task</h2>
+        </div>
+        
+    );
+};
+
+export default Todos;
